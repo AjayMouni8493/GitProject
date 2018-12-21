5d6b0a29939d5ac26a6a113ccfccccec7fc66b26
AjayMouni8493 <ankaiahm.tms@gmail.com>
2018-12-21 16:22:34 +0530

    GitProject-2

---
+++ app/src/main/java/example/android/com/gitproejct_2/MainActivity.java
@@ -0,0 +1,16 @@
+package example.android.com.gitproejct_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Git Project 222222222222222 ......................
+    }
+    
+}
